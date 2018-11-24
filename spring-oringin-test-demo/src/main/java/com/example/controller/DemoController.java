a046a1606e4d0490e3e26ffc100e02fe93ee72a6
¹þà½ßç <3108065@qq.com>
2018-11-24 23:44:07 +0800

    spring

---
+++ spring-oringin-test-demo/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+/**
+ * @author sh on 2018/11/24 22:42
+ * @version ideaIU-2018.2.3.win_home
+ */
+public class DemoController {
+    public void demo() {
+
+
+        
+    }
+}
---
+++ spring-oringin-test-demo/src/test/java/com/example/controller/DemoControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.junit.Test;
+
+
+/**
+ * @author sh on 2018/11/24 22:44
+ * @version ideaIU-2018.2.3.win_home
+ */
+public class DemoControllerTest {
+
+    @Test
+    public void demo() {
+    }
+}
